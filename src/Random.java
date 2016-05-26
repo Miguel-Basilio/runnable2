100dd0884bec1254706c2ceb4660cf427152f368
MiguelBasilio <lokoa@DESKTOP-F4BVPJT>
2016-05-25 20:36:54 -0700

    random game improve add one missing line random.setName......

--- src/Random.java
+++ src/Random.java
@@ -4,11 +4,17 @@ public class Random {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (int i=0; i<25; i++){
-			Runnable R1 = new MyThread(i);
+		for (int i=0; i<2; i++){
+			Runnable R1 = new MyThread(25);
 			Thread random = new Thread(R1);
-			
+			random.setName(String.valueOf(10));
 			random.start();
+			
+			
+			Runnable R2 = new MyThread(i*2);
+			Thread random2 = new Thread(R2);
+			random.setName(String.valueOf(10));
+			random2.start();
 		}
 	System.out.println("done");
 	}
